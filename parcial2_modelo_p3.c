dcc57937d578510be9f52e276faac7d1ea30ad2b
Federico Lucero <fedelucero95@gmail.com>
2024-06-24 20:12:53 -0300

    modelo parcial

--- parcial2_modelo_p3.c
+++ parcial2_modelo_p3.c
@@ -19,20 +19,33 @@ d) Informar los vendedores que no registraron ventas durante el día
 */
 #include <stdio.h>
 
-int IngresoDatos(int[], int[]);
-int MayorFacturacion(int, int);
-int MenorRecaudacion(int, int);
-int VendedoresSinVentas(int[], int[]);
+int IngresoDatos(int[], float[]);
+void MayorRecaudacion(float[]);
+void MenorRecaudacion(float[]);
+void VendedoresSinVentas(float[]);
 
 int main() {
-    int vendedor, importe, maxRecaudacion, minRecaudacion, topVendedor, lowVendedor, flag = 0;
     int vecVendedor[20] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
-    int vecVentas[20] = {0};
-    IngresoDatos(vendedor, importe);
-    printf("%d - %d", vendedor, importe);
+    int cont = 0;
+    int validar = 0;
+    float vecVentas[20] = {0};
+    float maxRecaudacion;
+
+    do{
+        validar = IngresoDatos(vecVendedor, vecVentas);
+        cont++;
+    } while (validar == 0);
+
+    if (cont > 1){
+        MayorRecaudacion(vecVentas);
+        MenorRecaudacion(vecVentas);
+        VendedoresSinVentas(vecVentas);
+    }
+
+    return 0;
 }
 
-int IngresoDatos(int v1[20], int v2[20]) {
+int IngresoDatos(int v1[20], float v2[20]) {
     int vendedor;
     float importe;
     do {
@@ -50,4 +63,42 @@ int IngresoDatos(int v1[20], int v2[20]) {
     } while (importe < 0);
 
     v2[vendedor - 1] += importe;
+    return 0;
+}
+
+void MayorRecaudacion(float v2[20]) {
+    int flag = 0, maxVendedor;
+    float max = 0;
+    for(int i = 0; i < 20; i++) {
+        if (flag == 0 || v2[i] > max) {
+            max = v2[i];
+            flag = 1;
+            maxVendedor = i + 1;
+        }
+    }
+
+    printf("\nEl vendedor %d cerro el dia con la mayor recaudacion, un total de: $%.2f\n", maxVendedor, max);
+}
+
+void MenorRecaudacion(float v2[20]) {
+    int vendedorLow, flag = 0;
+    float menorRec = 0;
+    
+    for (int i = 0; i < 20; i++){
+        if((flag == 0 || v2[i] < menorRec) && v2[i] > 0) {
+            menorRec = v2[i];
+            vendedorLow = i + 1;
+            flag = 1;
+        }
+    }
+
+    printf("El vendedor %d fue el que menos recaudo, con un total de $%.2f\n", vendedorLow, menorRec);
+}
+
+void VendedoresSinVentas(float v2[20]){
+    for(int i = 0; i < 20; i++) {
+        if(v2[i] == 0) {
+            printf("El vendedor %d no hizo ventas.\n", i + 1);
+        }
+    }
 }
